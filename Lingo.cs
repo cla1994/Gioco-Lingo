4327c50e930b3a841d6d4064fd32076616aae542
ClaudioMenichini1994 <claudiomenichini94@gmail.com>
2023-06-09 12:58:41 +0200

    Aggiungere i file di progetto.

---
+++ Lingo.cs
@@ -0,0 +1,130 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+
+namespace Day6Lab1 {
+    public class Lingo {
+
+        public string ParolaDaAzzeccare;
+        public string ParolaFinoAdOra { get; set; } 
+        private List<bool> ListaUltimaConfigBooleani { get; set; }
+
+        public string StringaContiene;
+
+        private string[] GruppoParolePossibili = {
+            "nequizia","livore","foriero","prodromo","astruso","mendace","melologo","lutolento" 
+        };
+
+        public char EvitareMessaggioLettereDoppie = ' ';
+
+        public int TentativiRimanenti = 5;
+
+        public Lingo() {
+
+            ListaUltimaConfigBooleani = new List<bool>();    
+        } 
+        public void SettaParolaDaIndovinare() {
+
+            List<bool> ParolaAllInizio = new List<bool>();
+
+            var MioGruppoParole = (from ParolaPossibile in GruppoParolePossibili
+                        select ParolaPossibile).ToList();
+
+            Random rnd = new Random();
+
+            ParolaDaAzzeccare = MioGruppoParole[rnd.Next(0,7)];
+            
+            for (int i = 0; i < ParolaDaAzzeccare.Length; i++) 
+            { 
+                if (i != 0)
+                {
+                    ParolaAllInizio.Add(false);
+                }
+                else
+                {
+                    ParolaAllInizio.Add(true);
+                }
+            }
+
+            MostraParola(ParolaAllInizio);       
+        }
+
+        public string ContieneLetteraDaQualcheParte(string ParolaTentata)                               
+        {
+
+            for (int i = 0; i < ParolaTentata.Length; i++)    
+            {
+                if (ParolaDaAzzeccare.Contains(ParolaTentata[i]) && ParolaTentata[i] != ParolaDaAzzeccare[i] && EvitareMessaggioLettereDoppie != ParolaTentata[i])
+                {                                                               // primo &&: così lettere giuste al posto giusto non le segnala, già scritte
+                    StringaContiene += ParolaTentata[i];                        // secondo &&: così per esempio non scrive 2 volte "la z è al posto sbagliato" se parola tentata ha 2 z
+
+                    EvitareMessaggioLettereDoppie = ParolaTentata[i];
+                }                                                                                                   
+            }
+
+            return StringaContiene;
+        }
+
+         public void MostraParola(List<bool> ListaBooleaniIndovinatiENon) 
+         {
+            
+            string ParolaAQuestoTentativo = "";
+
+            for (int j = 0; j < ListaUltimaConfigBooleani.Count; j++ )  
+            {
+
+                if (ListaUltimaConfigBooleani[j]==true) {
+                    ListaBooleaniIndovinatiENon[j] = true;
+                }
+            } 
+        
+            for (int k = 0; k < ListaBooleaniIndovinatiENon.Count; k++)
+            {
+
+                if (ListaBooleaniIndovinatiENon[k] == true) 
+                {
+                    ParolaAQuestoTentativo = ParolaAQuestoTentativo + ParolaDaAzzeccare[k];
+                }
+                else
+                {
+                    ParolaAQuestoTentativo = ParolaAQuestoTentativo + "?";          // "□"
+                }
+            }
+
+            ParolaFinoAdOra = ParolaAQuestoTentativo;                // fa doppio salvataggio di valori calcolati in funzione
+
+            ListaUltimaConfigBooleani = ListaBooleaniIndovinatiENon;    
+        }
+
+        public List<bool> QualiLettereGiusteENon(string ParolaTentata)  
+        {
+
+            List<bool> ListaBooleaniTrovatiENon = new List<bool>();
+
+            for (int k = 0; k < ParolaTentata.Length;k++)
+            {
+                ListaBooleaniTrovatiENon.Add(false); // true o false è lo stesso tanto lo cambio, importante è crearla con tot
+            }                                           // elementi così posso intervenire sui singoli (necessario per lettere
+                                                        // ripetute che non compaiono)
+
+            for (int i = 0; i < ParolaTentata.Length; i++)
+            {
+                if (ParolaTentata[i] == ParolaDaAzzeccare[i])
+                {
+                    ListaBooleaniTrovatiENon[i] = true;
+
+                 /*   for (int j = 0; j < ParolaTentata.Length; j++)            // commentata questa parte se non voglio che se parola è fama e io dico fame non metta entrambe le a
+                    {
+                        if (ParolaTentata[i] == ParolaDaAzzeccare[j])               // se lettera (con i che rimane fermo) compare ancora
+                        {                                                                // nella parola da azzeccare, scrive anche lei
+                            ListaBooleaniTrovatiENon[j] = true;
+                        }
+                    }   */                                                         // non c'è bisogno fare caso else a parolatentata == paroladaazzeccare; inizializzato tutto a 
+                }                                                                   // false, semplicemente rende true quando trova lettera giusta in posizione giusta e poi mette true
+            }                                                               //altre lettere uguali; mettendo else lista[i] = false c'era problema che sovrascriveva lettere giuste
+                                                                     // che si ripetevano (tipo se azzeccavo la i in "nequizia" la seconda i veniva sovrascritta perché condizione
+            MostraParola(ListaBooleaniTrovatiENon);             // if non soddisfatta
+
+            return ListaBooleaniTrovatiENon;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Day6Lab1.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+
+        private Lingo GiocoLingo;
+        public string ParolaFinoAdOra { get; set; }
+
+        public bool Vittoria = false;
+
+        public int NumeroTentativi;
+
+        public bool Sconfitta = false;
+
+        public List<bool> ListaBooleaniIndovinati { get; set; }
+
+        public string ListaAppareDaQualcheParte = "";
+
+        public IndexModel(ILogger<IndexModel> logger,Lingo Gioco) {
+
+            _logger = logger;
+            
+            GiocoLingo = Gioco;
+
+            ParolaFinoAdOra = Gioco.ParolaFinoAdOra;
+
+            ListaAppareDaQualcheParte = Gioco.StringaContiene;
+
+            NumeroTentativi = Gioco.TentativiRimanenti - 1;
+
+         ///   ListaBooleaniIndovinati = new List<bool>();
+
+        }
+
+        public void OnGet() {
+
+            GiocoLingo.SettaParolaDaIndovinare();
+            ParolaFinoAdOra = GiocoLingo.ParolaFinoAdOra;
+
+        }
+
+        public  void OnPost(string? ParolaProvata) {
+
+            if (ParolaProvata != null) {
+
+                ListaBooleaniIndovinati = GiocoLingo.QualiLettereGiusteENon(ParolaProvata);
+                ListaAppareDaQualcheParte = GiocoLingo.ContieneLetteraDaQualcheParte(ParolaProvata);
+                ParolaFinoAdOra = GiocoLingo.ParolaFinoAdOra;
+
+                if (!ListaBooleaniIndovinati.Contains(false))
+                {
+                    Vittoria = true;
+                }
+
+                GiocoLingo.TentativiRimanenti--;
+                if (GiocoLingo.TentativiRimanenti == 0)
+                {
+                    Sconfitta = true;
+                }
+
+            }
+        }
+    }
+}
